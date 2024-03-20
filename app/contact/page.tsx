f1012fb6256cce8e54e51899bfbbc7e31c64cc69
shintaro <shintaro20020415@gmail.com>
2024-03-20 16:17:35 +0900

    muiのインストールと問い合わせページのフロント部分を実装

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,27 @@
+import { FormControl, TextField, Stack, Box, Button, Typography } from '@mui/material';
+
+// とい
 export default function Page() {
-    return <p>Contact Page</p>
+    return (
+        <Box bgcolor="white" padding={4} borderRadius={3}>
+            <Typography color="black" align="center" variant="h2" mb={1}>
+                Contact
+            </Typography>
+            <Box width={600} m="0 auto">
+                <FormControl fullWidth>
+                    <Stack spacing={2} direction="column">
+                        <TextField required label="会社名" />
+                        <TextField required label="お名前" />
+                        <TextField required label="メールアドレス" type='email' />
+                        <TextField required label="お問い合わせ内容" multiline rows={6} />
+                    </Stack>
+                    <Stack mt={2} alignItems="center">
+                        <Button>
+                            送信
+                        </Button>
+                    </Stack>
+                </FormControl>
+            </Box>
+        </Box>
+    );
 }
