361eb8b9b03b0c3a0f1544fd33115db39f0f8793
shintaro <shintaro20020415@gmail.com>
2024-03-30 16:24:46 +0900

    topページのレスポンシブ対応

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,21 @@ import Link from "next/link";
 export default function Page() {
   return (
       <Box component="div">
-        <Box borderLeft={3} paddingLeft={4} paddingY={2}>
+        <Box component="div" className="p-7 border-l-[3px] border-current">
           <Typography variant="h1" className="animate-slide-in-top tracking-wide">
+            <Box component="div" className="text-4xl sm:text-8xl">
               Hello, I&apos;m<br/>Shintaro Masui
+            </Box>
           </Typography>
-          <Box component="div" className="animate-text-focus-in justify-between" display="flex" maxWidth={630} alignItems="center" py={2}>
+          <Box component="div" className="animate-text-focus-in sm:flex sm:justify-between" maxWidth={630} alignItems="center" py={2}>
             <Box component="div">
               <Typography variant="body1">A university student</Typography>
               <Typography variant="body1">Apprentice Web developer</Typography>
               <Typography variant="body1">Living Oosaka in Japan</Typography>
             </Box>
-            <Box component="div" display="flex" height={30}>
+            <Box component="div" className="flex justify-center sm:mt-0 mt-8">
               <Link href="/about" className="hover:opacity-60">
-                  {"Who is shintaro ? "}
+                  {"Detail me"}
                   <ArrowForwardIosIcon/>
               </Link>
             </Box>
